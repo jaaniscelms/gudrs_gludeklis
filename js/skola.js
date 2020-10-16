774392dfe393aebed32962e2a19c5424ad65a8f9
inesesv <inese.vilumsone@citadaskola.lv>
2020-10-16 13:15:30 +0300

    Pievienota javascripta datne, kas ļauj izveidot jaunu tabulas rindu

---
+++ js/skola.js
@@ -0,0 +1,27 @@
+const add = document.getElementById('pievieno');
+const save1 = document.getElementById('save');
+var a,b,c,d;
+add.addEventListener('click', function() {
+    
+    var tabula = document.getElementById("pasniedzejs"); 
+    var rinda = document.createElement("tr")
+    rinda.innerHTML = `
+       <td><input id="dati0" class ="dati" type="text"></td>
+       <td><input id="dati1" class ="dati" type="text"></td>
+       <td><input id="dati2"class ="dati" type="text"></td>
+       <td><input id="dati3"class ="dati" type="text"></td>
+       <td><input class="button" onclick="save()" type="button" value="Saglabāt" ></td>
+    `;            
+    tabula.appendChild(rinda);
+    add.style.display = "none";
+});
+function save(){
+        var tabula = document.getElementById("pasniedzejs"); 
+    var rinda = tabula.rows[ tabula.rows.length - 1 ];
+    for(i=0;i<4;i++){
+         rinda.cells[i].innerHTML=document.getElementById("dati"+i).value;
+
+    }
+    rinda.cells[4].innerHTML = '<div class="buttons"><input class="button" type="button" value="Labot"><input class="button button_error" type="button" value="Dzēst">  </div>';
+    add.style.display = "block";
+}
