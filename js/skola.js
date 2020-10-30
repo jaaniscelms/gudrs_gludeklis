683ba6113b006f50709cac3de5002e94d606d501
EdKuGitHub <70010771+EdKuGitHub@users.noreply.github.com>
2020-10-30 23:41:17 +0200

    Pierakstīts kods lai nolasītu datus no skola.json

--- js/skola.js
+++ js/skola.js
@@ -1,4 +1,59 @@
-document.getElementById('pievieno').addEventListener('click', function(){
+//Iegūstam datus no API 
+async function iegutSkolotajuDatusNoApi()
+{
+    let datiNoApi = await fetch('json/skola.json')
+    let datiJson = await datiNoApi.json();
+    return datiJson;
+}
+//Datu izvadīšana
+async function izvaditDatus()
+{
+    let tableMainPart = "";
+    let teachersTable = "";
+    //Tabulas augšdaļa.
+    let tableUpPart = `
+    <h2>Pasniedzēji</h2>
+    <table>
+      <thead>
+        <tr>
+          <th class="vards">Vārds, uzvārds</th>
+          <th class="epasts">E-pasts</th>
+          <th class="tel">Tel. nr.</th>
+          <th class="prieksmets">Priekšmets</th>
+          <th>Darbības</th>
+        </tr>
+      </thead>
+      <tbody>`;
+      //Tabulas apakšdaļa.
+      let tableEndPart = `
+      </tbody>
+          </table>
+          <input class="button" type="button" id="pievieno" value="Pievienot pasniedzēju" onclick="pievienotTeacher()">`;  
+    let teachersList = await iegutSkolotajuDatusNoApi();
+    for (let index = 0; index < teachersList["pasniedzeji"].length; index++) 
+    {
+      tableMainPart += `
+      <tr>
+      <td class="vards">`+ teachersList["pasniedzeji"][index].vards +" "+ teachersList["pasniedzeji"][index].uzvards + `</td>
+      <td class="epasts">`+ teachersList["pasniedzeji"][index].epasts +`</td>
+      <td class="tel">`+ teachersList["pasniedzeji"][index].tel_num +`</td>
+      <td class="prieksmets">`+ teachersList["pasniedzeji"][index].prieksmets +`</td>
+      <td>
+        <div class="buttons">
+          <input class="button" type="button" value="Labot">
+          <input class="button button_error" type="button" value="Dzēst">
+        </div>
+      </td>
+      </tr>`         
+    }
+    teachersTable = tableUpPart.concat(tableMainPart,tableEndPart);
+    document.getElementById("teachers").innerHTML = teachersTable;      
+}
+izvaditDatus();
+
+//Cita pierakstīts kods
+/*document.getElementById('pievieno').addEventListener('click', */
+function pievienotTeacher(){
   const table_body = document.querySelector(".teachers tbody");
   var rinda = document.createElement("tr");
   rinda.innerHTML = `
@@ -30,4 +85,4 @@ document.getElementById('pievieno').addEventListener('click', function(){
       `;
     }
   })
-});
+};
