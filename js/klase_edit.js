94ad2453d13bfd04732b7fb29fb40dcc27997934
inesesv <70010678+inesesv@users.noreply.github.com>
2020-10-31 23:22:58 +0200

    Labots Json

--- js/klase_edit.js
+++ js/klase_edit.js
@@ -6,14 +6,15 @@ rinda[4]= document.querySelector('.ceturtdiena');
 rinda[5]= document.querySelector('.piektdiena');
 async function iegutDatus()
 {
-    let datiNoApi = await fetch('http://janiscelms.com/gudrs_gludeklis/json/klase_edit.json')
+    let datiNoApi = await fetch('json/klase_edit.json')
     let datiJson = await datiNoApi.json();
+alert(datiJson);
     return datiJson;
 }
 async function raditDatus()
 {
-    
-    let stundasJson = await iegutDatus();
+   
+    let stundasJson = await iegutDatus;
     for (let i = 0; i < stundasJson["stundas"].length; i++) 
     {
      rinda[stundasJson["stundas"][i].diena].innerHTML+=`
