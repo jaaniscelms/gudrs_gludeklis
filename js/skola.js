a75ada8cc102f30bdc83ef11ba89e7c2ff832d1a
Jānis Celms <janis.celms@eazybi.com>
2020-11-01 00:32:09 +0200

    Nedaudz vienkarso skola js tabulas aizpildi

--- js/skola.js
+++ js/skola.js
@@ -1,59 +1,28 @@
-//Iegūstam datus no API 
-async function iegutSkolotajuDatusNoApi()
-{
-    let datiNoApi = await fetch('http://janiscelms.com/gudrs_gludeklis/json/skola.json')
-    let datiJson = await datiNoApi.json();
-    return datiJson;
-}
-//Datu izvadīšana
-async function izvaditDatus()
-{
-    let tableMainPart = "";
-    let teachersTable = "";
-    //Tabulas augšdaļa.
-    let tableUpPart = `
-    <h2>Pasniedzēji</h2>
-    <table>
-      <thead>
-        <tr>
-          <th class="vards">Vārds, uzvārds</th>
-          <th class="epasts">E-pasts</th>
-          <th class="tel">Tel. nr.</th>
-          <th class="prieksmets">Priekšmets</th>
-          <th>Darbības</th>
-        </tr>
-      </thead>
-      <tbody>`;
-      //Tabulas apakšdaļa.
-      let tableEndPart = `
-      </tbody>
-          </table>
-          <input class="button" type="button" id="pievieno" value="Pievienot pasniedzēju" onclick="pievienotTeacher()">`;  
-    let teachersList = await iegutSkolotajuDatusNoApi();
-    for (let index = 0; index < teachersList["pasniedzeji"].length; index++) 
-    {
-      tableMainPart += `
-      <tr>
-      <td class="vards">`+ teachersList["pasniedzeji"][index].vards +" "+ teachersList["pasniedzeji"][index].uzvards + `</td>
-      <td class="epasts">`+ teachersList["pasniedzeji"][index].epasts +`</td>
-      <td class="tel">`+ teachersList["pasniedzeji"][index].tel_num +`</td>
-      <td class="prieksmets">`+ teachersList["pasniedzeji"][index].prieksmets +`</td>
-      <td>
-        <div class="buttons">
-          <input class="button" type="button" value="Labot">
-          <input class="button button_error" type="button" value="Dzēst">
-        </div>
-      </td>
-      </tr>`         
-    }
-    teachersTable = tableUpPart.concat(tableMainPart,tableEndPart);
-    document.getElementById("teachers").innerHTML = teachersTable;      
-}
-izvaditDatus();
+// Aizpilda teachers tbody ar skola.json skolotaju datiem
+fetch('/json/skola.json')
+  .then(response => response.json())
+  .then(data => data.forEach(element => {
+      let tr = document.createElement("tr");
 
-//Cita pierakstīts kods
-/*document.getElementById('pievieno').addEventListener('click', */
-function pievienotTeacher(){
+      tr.innerHTML = `
+        <td>${element.vards} ${element.uzvards}</td>
+        <td>${element.epasts}</td>
+        <td>${element.tel_num}</td>
+        <td>${element.prieksmets}</td>
+        <td>
+          <div class="buttons">
+            <input class="button" type="button" value="Labot">
+            <input class="button button_error" type="button" value="Dzēst">
+          </div>
+        </td>
+      `
+
+      document.querySelector('.teachers tbody').appendChild(tr)
+    })
+  )
+
+// Pievienot pasniedzeju
+document.getElementById('pievieno').addEventListener('click', function(){
   const table_body = document.querySelector(".teachers tbody");
   var rinda = document.createElement("tr");
   rinda.innerHTML = `
@@ -85,4 +54,4 @@ function pievienotTeacher(){
       `;
     }
   })
-};
+});
