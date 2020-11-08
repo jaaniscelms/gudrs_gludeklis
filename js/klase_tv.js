17dbfa385d045bf6ad35079335ecd187af4c4b40
Natallia <natallia.karatun@gmail.com>
2020-11-08 09:19:50 +0200

    Izlabots fails

--- js/klase_tv.js
+++ js/klase_tv.js
@@ -1,30 +1,14 @@
 fetch('/json/klase_tv.json')
   .then(response => response.json())
   .then(data => data.forEach(element =>
-    document.querySelector('.stundas tbody').innerHTML += `
-      <tr>
-        <td>${element.stunda}</td>
-        <td>${element.laiks}</td>
-      </tr>`
+    document.querySelector('.stundas').innerHTML += `
+      <ul>
+        <li>${element.stunda}</li>
+        <li>${element.laiks}</li>
+        <li>${element.nenotiek}</li>
+        <li>${element.patreiz_notiek}</li>
+      </ul>`
     )
   )
 
-document.getElementById('pievieno').addEventListener('click', function(){
-  const table_body = document.querySelector(".stundas tbody");
-  var rinda = document.createElement("tr");
-  rinda.innerHTML = `
-    <td><input type="text" placeholder="Stunda"></td>
-    <td><input type="text" placeholder="Laiks"></td>
-  `;
-  table_body.appendChild(rinda);
-  rinda.classList.add('row_edit')
 
-  document.querySelectorAll(".save").forEach(function(currentIndex){
-    currentIndex.onclick = function(){
-      for(i=0;i<8;i++){
-        var cells = this.closest('tr').children[i]
-        cells.innerHTML = cells.querySelector('input').value
-      }
-    }
-  })
-});
