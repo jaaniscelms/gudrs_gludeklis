2f91c05391c5437fec18ad09044296fbd4a0ad8a
inesesv <70010678+inesesv@users.noreply.github.com>
2021-03-14 22:15:53 +0200

    Noņemta mape venv
    mazliet palabots, noņemta mape venv

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, redirect, url_for,render_template, request
+import os
+from flask import Flask, redirect, url_for,render_template,request
 
 app=Flask(__name__)
+
 
 @app.route("/")
 def home():
@@ -10,27 +12,37 @@ def home():
 def klase_tv():
     return render_template("klase_tv.html")
 
-@app.route("/klase_edit")
-def klase_edit():
-    return render_template("klase_edit.html")
-
 @app.route("/klases")
 def klases():
+
     return render_template("klases.html")
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method=="POST":
+        usr=request.form["user"]
+        pasw=request.form["password"]
+        if usr=="admin" and pasw=="admin":
+             return redirect(url_for("skola"))
+        else: 
+            return redirect(url_for("login"))
+    else: 
+     return render_template("login.html")
+headings=("Nr.","Priekšmets", "Pasniedzejs", "Laiks","Nenotiek","Darbības")
+data=(
+    (1,"Vēsture","Andris Bērziņš","9:15 - 9:55","unchecked"),
+    (2, "Sports","Pēteris Skuja","10:15 - 10:55","checked"),
+    (3,"Ekonomika","Anna Panna","11:20 - 12:05", "unchecked"),
+    (4,"Matemātika","Valdis Liepa","12:35 - 13:05","checked"),
+    (5,"Mājturība","Elita Bērziņa","12:20 - 14:00", "checked")
+)
+@app.route("/klase_edit")
+def klase_edit():
+    return render_template("klase_edit.html",headings=headings, data=data)
 
 @app.route("/skola")
 def skola():
     return render_template("skola.html")
-
-
-
-@app.route("/login", methods=["POST", "GET"])
-def login():
-    if request.method == "POST":
-        user = request.form["user"]
-        return redirect(url_for("skola"))
-    else:
-        return render_template("login.html")
 
 if __name__=="__main__":
     app.run(debug=True)
--- templates/README.md
+++ templates/README.md
@@ -3,3 +3,5 @@
 ![alt text](img/kopskats.png "Gudrs Gludeklis projekta kopskats")
 
 Projekta augstākā hierarhija ir viena skola -> katra skola kas vēlēsies šo projektu izmantot, varēs paņemt savu projekta kopiju un tālāk jau paši hostēt un modificēt. Projekts ietver, datubāzi ar skolas skolotājiem, klasēm un katras klases stundu sarakstu nedēļai. Projektu paredzēts izmantot skolēniem/skolotājiem telefonos, datoros un uz kopēja tv ekrāna klasēs, kā arī projekta uzturētājiem izmantojot datoru un vēlams telefonu.
+
+lai projektu darbinātu uz datora ir jābūt instalētam: Flask un Python
