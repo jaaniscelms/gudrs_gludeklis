c352ca98d88861fcf9d871604f34a9e551ac3111
inesesv <70010678+inesesv@users.noreply.github.com>
2021-03-18 22:23:42 +0200

    Datu bāzes izveide
    database.py - pagaidu datne datubazes izveidei;
    dati.db - izveidotā datubāze, bija problēmas ar foreign key, bet apvienot izdodas arī bez tām;
    app.py - pagaidām izmeģināju vai izdodas pieslēgties datu bāzei un iegūt datus.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask, redirect, url_for,render_template,request
+from flask import Flask, redirect, url_for,render_template,request, json, jsonify
+import sqlite3
 
 app=Flask(__name__)
 
@@ -35,6 +36,33 @@ def klase_edit():
 
 @app.route("/skola")
 def skola():
+    
+    """ try:
+       with sqlite3.connect("Dati.db") as conn:
+        conn=sqlite3.connect('Dati.db')
+        c = conn.cursor()
+        c.execute("SELECT  pasniedzejs.vards, pasniedzejs.uzvards, pasniedzejs.epasts, pasniedzejs.telefons, prieksmeti.prieksmets FROM pasniedzejs JOIN prieksmeti ON pasniedzejs.prieksmeta_id = prieksmeti.prieksmeta_id ")
+        data = c.fetchall()
+        jsonData = ''
+        column_names = ['vards','uzvards','epasts','telefons','prieksmets']
+        for row in data:          
+            info = dict(zip(column_names, row))
+            jsonData = jsonData + json.dumps(info) + ','
+        jsonData = jsonData[:-1]
+        jsonData = '[' + jsonData + ']'
+        msg = "Ieraksti veiksmīgi saņemti un apstrādāti"
+        print(msg)
+    except:
+      conn.rollback()
+      msg = "Ir notikusi kļūda datu saņemšanā un apstrādāšanā"
+      print(msg)
+    finally:
+      conn.commit()
+      c.close()
+      conn.close()    
+      return jsonData
+
+"""
     return render_template("skola.html")
 
 if __name__=="__main__":
